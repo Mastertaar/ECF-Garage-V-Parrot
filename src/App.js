21a8f2ea178ea1a67840330c5ac4c2008431ac4c
Romain Pitarque <mastertaar_97@hotmail.com>
2023-07-20 10:41:37 +0200

    modified .gitignore & installed Node_module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Layout from './Components/Layout';
-import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom';
+import Planning from '../Planning/Planning';
+import Preferences from '../Preferences/Preferences';
 
 
 function App() {
---
+++ src/Components/Planning/Planning.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Planning() {
+  return(
+    <h2>Planning</h2>
+  );
+}
