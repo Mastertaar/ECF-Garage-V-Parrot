341b4d015bc7af8bd55d8083509918ed59362280
Romain Pitarque <mastertaar_97@hotmail.com>
2023-07-20 14:10:32 +0200

    Login.js & Css created + added token check and useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Layout from './Components/Layout';
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import Planning from '../src/Components/Planning/Planning';
+import Login from '../Login/Login';
 import Preferences from '../src/Components/Preferences/Preferences';
 
 
 
 function App() {
+  const [token, setToken] = useState();
+  
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
 
-   
 
     <div className="wrapper">
       <h1>Application</h1>
