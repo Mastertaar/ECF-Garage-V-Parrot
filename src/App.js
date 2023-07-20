44a9183ee8c047f7ae7aa7bc5f3790dbe72d7606
Romain Pitarque <mastertaar_97@hotmail.com>
2023-07-20 13:38:50 +0200

    Big Change BrowserRouter App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './App.css';
 import Layout from './Components/Layout';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from '../Dashboard/Dashboard';
-import Preferences from '../Preferences/Preferences';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import Planning from '../src/Components/Planning/Planning';
+import Preferences from '../src/Components/Preferences/Preferences';
 
 
 
 function App() {
   return (
+
+   
+
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route path="/planning">
+              <Planning />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </Layout>
       </BrowserRouter>
     </div>
+    
   );
 }
 
